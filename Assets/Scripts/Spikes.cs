8c57bd65cba6e4ef1684c9f5b8db97273cdeef05
Marcos Engelhard <engelhardmarcos2015@gmail.com>
2021-02-26 22:02:15 +0000

    Implementação de spikes

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Hit enemy!");
+        }
+    }
+}
